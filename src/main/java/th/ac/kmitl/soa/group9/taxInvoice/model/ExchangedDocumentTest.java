f03cfbfdb456d0126a3e27da2c73ba971ae117ef
tiwipabmin <tiwipabmin@gmail.com>
2018-03-06 22:56:51 +0700

    delete comment on the ExchangedDocumentTest.java file, IncludedNoteTest.java file and HomeController.java file

--- src/main/java/th/ac/kmitl/soa/group9/taxInvoice/model/ExchangedDocumentTest.java
+++ src/main/java/th/ac/kmitl/soa/group9/taxInvoice/model/ExchangedDocumentTest.java
@@ -15,25 +15,4 @@ public class ExchangedDocumentTest {
     private Timestamp creationDateTime = new Timestamp(System.currentTimeMillis());
     private IncludedNote includedNote = new IncludedNote();
 
-//    @Test
-//    public void shouldGetAllInfo() {
-//        ExchangedDocument exchangedDocument = new ExchangedDocument();
-//        exchangedDocument.setId(this.id);
-//        exchangedDocument.setTypeCode(this.typeCode);
-//        exchangedDocument.setIssueDateTime(this.issueDateTime);
-//        exchangedDocument.setPurpose(this.purpose);
-//        exchangedDocument.setPurposeCode(this.purposeCode);
-//        exchangedDocument.setGlobalId(this.globalId);
-//        exchangedDocument.setCreationDateTime(this.creationDateTime);
-//        exchangedDocument.setIncludedNote(this.includedNote);
-//
-//        assertEquals(this.id, exchangedDocument.getId());
-//        assertEquals(this.typeCode, exchangedDocument.getTypeCode());
-//        assertEquals(this.issueDateTime, exchangedDocument.getIssueDateTime());
-//        assertEquals(this.purpose, exchangedDocument.getPurpose());
-//        assertEquals(this.purposeCode, exchangedDocument.getPurposeCode());
-//        assertEquals(this.globalId, exchangedDocument.getGlobalId());
-//        assertEquals(this.creationDateTime, exchangedDocument.getCreationDateTime());
-//        assertEquals(this.includedNote, exchangedDocument.getIncludedNote());
-//    }
 }
--- src/main/java/th/ac/kmitl/soa/group9/taxInvoice/model/IncludedNoteTest.java
+++ src/main/java/th/ac/kmitl/soa/group9/taxInvoice/model/IncludedNoteTest.java
@@ -4,13 +4,4 @@ public class IncludedNoteTest {
     private String subject = "หมายเหตุ";
     private String content = "";
 
-//    @Test
-//    public void shouldGetAllInfo() {
-//        IncludedNote includedNote = new IncludedNote();
-//        includedNote.setSubject(this.subject);
-//        includedNote.setContent(this.content);
-//
-//        assertEquals(this.subject, includedNote.getSubject());
-//        assertEquals(this.content, includedNote.getContent());
-//    }
 }
