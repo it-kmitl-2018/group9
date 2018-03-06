2313bf1e9ccb1d59d214aeaf38b1b72d110cc1d9
jabwrb <s8070128@kmitl.ac.th>
2018-03-06 11:22:52 +0700

    Create ExchangedDocument, IncludedNote model and test

---
+++ src/main/java/th/ac/kmitl/soa/group9/taxInvoice/model/IncludedNote.java
@@ -0,0 +1,22 @@
+package th.ac.kmitl.soa.group9.taxInvoice.model;
+
+public class IncludedNote {
+    private String subject;
+    private String content;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
