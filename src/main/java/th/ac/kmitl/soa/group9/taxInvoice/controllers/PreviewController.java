4fe6fb2f24951bdfd538ed5ef742682af7b821de
belizwp <belizwp@live.com>
2018-03-11 16:22:34 +0700

    refactor
    change class name & move html to proper directory

--- src/main/java/th/ac/kmitl/soa/group9/taxInvoice/controllers/PreviewController.java
+++ src/main/java/th/ac/kmitl/soa/group9/taxInvoice/controllers/PreviewController.java
@@ -2,14 +2,12 @@ package th.ac.kmitl.soa.group9.taxInvoice.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class TaxInvoicePreviewController {
+public class PreviewController {
 
     @RequestMapping("/preview")
-    // @ResponseBody
-    public String index() {
-        return "preview_invoice.html";
+    public String preview() {
+        return "preview_taxinvoice";
     }
 }
