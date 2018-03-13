006eab9c07e2bbd5aa534fbe6932eec86118d07d
iamplaster <iamaplaster@gmail.com>
2018-03-13 10:39:55 +0700

    test TaxInvoiceController

--- src/main/java/th/ac/kmitl/soa/group9/taxInvoice/controllers/TaxInvoiceController.java
+++ src/main/java/th/ac/kmitl/soa/group9/taxInvoice/controllers/TaxInvoiceController.java
@@ -1,4 +1,4 @@
-package th.ac.kmitl.soa.group9.taxInvoice.controller;
+package th.ac.kmitl.soa.group9.taxInvoice.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/test/java/th/ac/kmitl/soa/group9/taxInvoice/controllers/TaxInvoiceControllerTest.java
@@ -0,0 +1,29 @@
+package th.ac.kmitl.soa.group9.taxInvoice.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TaxInvoiceController.class)
+public class TaxInvoiceControllerTest {
+
+    @Autowired
+    private MockMvc mMockMvc;
+
+    @Test
+    public void previewShouldReturnPreviewTaxInvoiceHtmlPage() throws Exception {
+        mMockMvc.perform(get("/home/index"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+
+}
