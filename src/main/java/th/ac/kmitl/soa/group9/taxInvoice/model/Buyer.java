61c42af6b8b63446ceb481a421cee11651e376bc
Weerabhat Meelarp <s8070128@kmitl.ac.th>
2018-03-15 02:14:10 +0700

    create Buyer model and test

---
+++ src/main/java/th/ac/kmitl/soa/group9/taxInvoice/model/Buyer.java
@@ -0,0 +1,59 @@
+package th.ac.kmitl.soa.group9.taxInvoice.model;
+
+public class Buyer {
+
+    private String id;
+    private String globalId;
+    private String name;
+    private String taxRegistrationId;
+    private ContactDetail contactDetail;
+    private Address address;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGlobalId() {
+        return globalId;
+    }
+
+    public void setGlobalId(String globalId) {
+        this.globalId = globalId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaxRegistrationId() {
+        return taxRegistrationId;
+    }
+
+    public void setTaxRegistrationId(String taxRegistrationId) {
+        this.taxRegistrationId = taxRegistrationId;
+    }
+
+    public ContactDetail getContactDetail() {
+        return contactDetail;
+    }
+
+    public void setContactDetail(ContactDetail contactDetail) {
+        this.contactDetail = contactDetail;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
---
+++ src/test/java/th/ac/kmitl/soa/group9/taxInvoice/model/BuyerTest.java
@@ -0,0 +1,33 @@
+package th.ac.kmitl.soa.group9.taxInvoice.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuyerTest {
+
+    private String id = "";
+    private String globalId = "";
+    private String name = "บริษัท ร่วมสมยัธุรกรรมอิเล็กทรอนิกส์ จำกัด";
+    private String taxRegistrationId = "057552600909900000";
+    private ContactDetail contactDetail = new ContactDetail();
+    private Address address = new Address();
+
+    @Test
+    public void shouldGetAllInfo() {
+        Buyer buyer = new Buyer();
+        buyer.setId(this.id);
+        buyer.setGlobalId(this.globalId);
+        buyer.setName(this.name);
+        buyer.setTaxRegistrationId(this.taxRegistrationId);
+        buyer.setContactDetail(this.contactDetail);
+        buyer.setAddress(this.address);
+
+        assertEquals(this.id, buyer.getId());
+        assertEquals(this.globalId, buyer.getGlobalId());
+        assertEquals(this.name, buyer.getName());
+        assertEquals(this.taxRegistrationId, buyer.getTaxRegistrationId());
+        assertEquals(this.contactDetail, buyer.getContactDetail());
+        assertEquals(this.address, buyer.getAddress());
+    }
+}
