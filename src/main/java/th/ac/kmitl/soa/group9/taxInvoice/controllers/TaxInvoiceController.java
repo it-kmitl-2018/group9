7d1a0a5d19b5536c07890502d3f18ed644f214fd
tiwipabmin <tiwipabmin@gmail.com>
2018-03-18 00:18:17 +0700

    create method createTaxInvoice for send tax invoice information from home page to show on preview page

--- src/main/java/th/ac/kmitl/soa/group9/taxInvoice/controllers/TaxInvoiceController.java
+++ src/main/java/th/ac/kmitl/soa/group9/taxInvoice/controllers/TaxInvoiceController.java
@@ -1,14 +1,24 @@
 package th.ac.kmitl.soa.group9.taxInvoice.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import th.ac.kmitl.soa.group9.taxInvoice.models.ExchangedDocument;
 
 @Controller
 public class TaxInvoiceController {
 
     @RequestMapping("/home/index")
-    public String taxInvoiceForm() { // Mapping GET Request and call home-index html page
+    public String taxInvoiceForm(Model model) { // Mapping GET Request and call home-index html page
+        ExchangedDocument exchangedDocument = new ExchangedDocument();
+        model.addAttribute("exchangedDocument", exchangedDocument);
         return "home-index";
     }
 
+    @PostMapping(value = "/home/index", params = "create")
+    public String createTaxInvoice(@ModelAttribute ExchangedDocument exchangedDocument){
+        return "preview_taxinvoice";
+    }
 }
