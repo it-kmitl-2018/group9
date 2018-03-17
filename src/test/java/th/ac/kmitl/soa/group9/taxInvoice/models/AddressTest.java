09364b072606aa6f3879b7051acde6a00680833f
tiwipabmin <tiwipabmin@gmail.com>
2018-03-17 18:43:57 +0700

    move all files at package model to package models in test folder, delete ExchangedDocumentTest and IncludesTest at package models in main folder

--- src/test/java/th/ac/kmitl/soa/group9/taxInvoice/models/AddressTest.java
+++ src/test/java/th/ac/kmitl/soa/group9/taxInvoice/models/AddressTest.java
@@ -1,4 +1,4 @@
-package th.ac.kmitl.soa.group9.taxInvoice.model;
+package th.ac.kmitl.soa.group9.taxInvoice.models;
 
 import org.junit.jupiter.api.Test;
 import th.ac.kmitl.soa.group9.taxInvoice.models.Address;
