e3e74abbcbda0bf7ddbf7f4cd78e916763102a4a
belizwp <belizwp@live.com>
2018-03-19 15:11:45 +0700

    create TaxInvoiceForm class

---
+++ src/main/java/th/ac/kmitl/soa/group9/taxInvoice/forms/TaxInvoiceForm.java
@@ -0,0 +1,88 @@
+package th.ac.kmitl.soa.group9.taxInvoice.forms;
+
+import java.sql.Timestamp;
+
+public class TaxInvoiceForm {
+
+    public String id;
+    public String name;
+    public String typeCode;
+    public Timestamp issueDateTime;
+    public String purpose;
+    public String purposeCode;
+    public String globalID;
+    public String subject;
+    public String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTypeCode() {
+        return typeCode;
+    }
+
+    public void setTypeCode(String typeCode) {
+        this.typeCode = typeCode;
+    }
+
+    public Timestamp getIssueDateTime() {
+        return issueDateTime;
+    }
+
+    public void setIssueDateTime(Timestamp issueDateTime) {
+        this.issueDateTime = issueDateTime;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public String getPurposeCode() {
+        return purposeCode;
+    }
+
+    public void setPurposeCode(String purposeCode) {
+        this.purposeCode = purposeCode;
+    }
+
+    public String getGlobalID() {
+        return globalID;
+    }
+
+    public void setGlobalID(String globalID) {
+        this.globalID = globalID;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
