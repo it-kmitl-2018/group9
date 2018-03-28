56704935c98ec1675616f9728c57656754007604
Weerabhat Meelarp <s8070128@kmitl.ac.th>
2018-03-28 22:09:53 +0700

    Add new line between attributes

--- src/main/java/th/ac/kmitl/soa/group9/taxInvoice/models/json/Item.java
+++ src/main/java/th/ac/kmitl/soa/group9/taxInvoice/models/json/Item.java
@@ -8,10 +8,13 @@ public class Item {
 
     public String description;
     public int quantity;
+
     @JsonProperty("price_per_item")
     public float pricePerItem;
+
     @JsonProperty("have_vat")
     public boolean haveVat;
+
     @JsonProperty("vat_rate")
     public float vatRate;
 }
