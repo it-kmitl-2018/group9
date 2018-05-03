d755415de6da7d9c9208b7ec85ce40b468723d18
iamplaster <iamaplaster@gmail.com>
2018-05-04 01:46:01 +0700

    Add seller attributes in TaxInvoiceForm

--- src/main/java/th/ac/kmitl/soa/group9/taxinvoice/forms/TaxInvoiceForm.java
+++ src/main/java/th/ac/kmitl/soa/group9/taxinvoice/forms/TaxInvoiceForm.java
@@ -20,6 +20,23 @@ public class TaxInvoiceForm {
     private String globalId;
     private String subject;
     private String content;
+    private String sellerId;
+    private String sellerGlobalId;
+    private String sellerName;
+    private String sellerTaxRegId;
+    private String sellerBranchId;
+    private String sellerEmail;
+    private String sellerBuilding;
+    private String sellerBuildingNum;
+    private String sellerSoi;
+    private String sellerVillage;
+    private String sellerMoo;
+    private String sellerStreet;
+    private String sellerCitySub;
+    private String sellerCity;
+    private String sellerCountrySub;
+    private String sellerCountry;
+    private String sellerPostCode;
 
     public TaxInvoiceForm(String id,
                           String name,
@@ -29,7 +46,24 @@ public class TaxInvoiceForm {
                           String purposeCode,
                           String globalId,
                           String subject,
-                          String content) {
+                          String content,
+                          String sellerId,
+                          String sellerGlobalId,
+                          String sellerName,
+                          String sellerTaxRegId,
+                          String sellerBranchId,
+                          String sellerEmail,
+                          String sellerBuilding,
+                          String sellerBuildingNum,
+                          String sellerSoi,
+                          String sellerVillage,
+                          String sellerMoo,
+                          String sellerStreet,
+                          String sellerCitySub,
+                          String sellerCity,
+                          String sellerCountrySub,
+                          String sellerCountry,
+                          String sellerPostCode) {
         this.id = id;
         this.name = name;
         this.typeCode = typeCode;
@@ -39,6 +73,23 @@ public class TaxInvoiceForm {
         this.globalId = globalId;
         this.subject = subject;
         this.content = content;
+        this.sellerId = sellerId;
+        this.sellerGlobalId = sellerGlobalId;
+        this.sellerName = sellerName;
+        this.sellerTaxRegId = sellerTaxRegId;
+        this.sellerBranchId = sellerBranchId;
+        this.sellerEmail = sellerEmail;
+        this.sellerBuilding = sellerBuilding;
+        this.sellerBuildingNum = sellerBuildingNum;
+        this.sellerSoi = sellerSoi;
+        this.sellerVillage = sellerVillage;
+        this.sellerMoo = sellerMoo;
+        this.sellerStreet = sellerStreet;
+        this.sellerCitySub = sellerCitySub;
+        this.sellerCity = sellerCity;
+        this.sellerCountrySub = sellerCountrySub;
+        this.sellerCountry = sellerCountry;
+        this.sellerPostCode = sellerPostCode;
     }
 
     public TaxInvoiceForm() {
@@ -114,5 +165,141 @@ public class TaxInvoiceForm {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public String getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(String sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public String getSellerGlobalId() {
+        return sellerGlobalId;
+    }
+
+    public void setSellerGlobalId(String sellerGlobalId) {
+        this.sellerGlobalId = sellerGlobalId;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public String getSellerTaxRegId() {
+        return sellerTaxRegId;
+    }
+
+    public void setSellerTaxRegId(String sellerTaxRegId) {
+        this.sellerTaxRegId = sellerTaxRegId;
+    }
+
+    public String getSellerBranchId() {
+        return sellerBranchId;
+    }
+
+    public void setSellerBranchId(String sellerBranchId) {
+        this.sellerBranchId = sellerBranchId;
+    }
+
+    public String getSellerEmail() {
+        return sellerEmail;
+    }
+
+    public void setSellerEmail(String sellerEmail) {
+        this.sellerEmail = sellerEmail;
+    }
+
+    public String getSellerBuilding() {
+        return sellerBuilding;
+    }
+
+    public void setSellerBuilding(String sellerBuilding) {
+        this.sellerBuilding = sellerBuilding;
+    }
+
+    public String getSellerBuildingNum() {
+        return sellerBuildingNum;
+    }
+
+    public void setSellerBuildingNum(String sellerBuildingNum) {
+        this.sellerBuildingNum = sellerBuildingNum;
+    }
+
+    public String getSellerSoi() {
+        return sellerSoi;
+    }
+
+    public void setSellerSoi(String sellerSoi) {
+        this.sellerSoi = sellerSoi;
+    }
+
+    public String getSellerVillage() {
+        return sellerVillage;
+    }
+
+    public void setSellerVillage(String sellerVillage) {
+        this.sellerVillage = sellerVillage;
+    }
+
+    public String getSellerMoo() {
+        return sellerMoo;
+    }
+
+    public void setSellerMoo(String sellerMoo) {
+        this.sellerMoo = sellerMoo;
+    }
+
+    public String getSellerStreet() {
+        return sellerStreet;
+    }
+
+    public void setSellerStreet(String sellerStreet) {
+        this.sellerStreet = sellerStreet;
+    }
+
+    public String getSellerCitySub() {
+        return sellerCitySub;
+    }
+
+    public void setSellerCitySub(String sellerCitySub) {
+        this.sellerCitySub = sellerCitySub;
+    }
+
+    public String getSellerCity() {
+        return sellerCity;
+    }
+
+    public void setSellerCity(String sellerCity) {
+        this.sellerCity = sellerCity;
+    }
+
+    public String getSellerCountrySub() {
+        return sellerCountrySub;
+    }
+
+    public void setSellerCountrySub(String sellerCountrySub) {
+        this.sellerCountrySub = sellerCountrySub;
+    }
+
+    public String getSellerCountry() {
+        return sellerCountry;
+    }
+
+    public void setSellerCountry(String sellerCountry) {
+        this.sellerCountry = sellerCountry;
+    }
+
+    public String getSellerPostCode() {
+        return sellerPostCode;
+    }
+
+    public void setSellerPostCode(String sellerPostCode) {
+        this.sellerPostCode = sellerPostCode;
     }
 }
--- src/test/java/th/ac/kmitl/soa/group9/taxinvoice/forms/TaxInvoiceFormTest.java
+++ src/test/java/th/ac/kmitl/soa/group9/taxinvoice/forms/TaxInvoiceFormTest.java
@@ -18,6 +18,23 @@ public class TaxInvoiceFormTest {
     private String globalId = "ABCDEFGHIJKLMNOPQRST123456789012345";
     private String subject = "หมายเหตุ";
     private String content = "ค่าบริการเพิ่มเติม";
+    private String sellerId = "ABCDEFGHIJKLMNOPQRST123456789012345";
+    private String sellerGlobalId = "123456";
+    private String sellerName = "บริษัท ร่วมสมัยการค้าไทย จำกัด";
+    private String sellerTaxRegId = "0575526009099";
+    private String sellerBranchId = "00000";
+    private String sellerEmail = "tax@rstmarket.co.th";
+    private String sellerBuilding = "อื้อจือเหลียง";
+    private String sellerBuildingNum = "968/1";
+    private String sellerSoi = "ศาลาแดง 1";
+    private String sellerVillage = "ศาลาแดงวิลล่า";
+    private String sellerMoo = "3";
+    private String sellerStreet = "สีลม";
+    private String sellerCitySub = "101701";
+    private String sellerCity = "1017";
+    private String sellerCountrySub = "10";
+    private String sellerCountry = "TH";
+    private String sellerPostCode = "10310";
 
     @Test
     public void shouldGetAllInfo() {
@@ -33,6 +50,24 @@ public class TaxInvoiceFormTest {
         taxInvoiceForm.setSubject(this.subject);
         taxInvoiceForm.setContent(this.content);
 
+        taxInvoiceForm.setSellerId(this.sellerId);
+        taxInvoiceForm.setSellerGlobalId(this.sellerGlobalId);
+        taxInvoiceForm.setSellerName(this.sellerName);
+        taxInvoiceForm.setSellerTaxRegId(this.sellerTaxRegId);
+        taxInvoiceForm.setSellerBranchId(this.sellerBranchId);
+        taxInvoiceForm.setSellerEmail(this.sellerEmail);
+        taxInvoiceForm.setSellerBuilding(this.sellerBuilding);
+        taxInvoiceForm.setSellerBuildingNum(this.sellerBuildingNum);
+        taxInvoiceForm.setSellerSoi(this.sellerSoi);
+        taxInvoiceForm.setSellerVillage(this.sellerVillage);
+        taxInvoiceForm.setSellerMoo(this.sellerMoo);
+        taxInvoiceForm.setSellerStreet(this.sellerStreet);
+        taxInvoiceForm.setSellerCitySub(this.sellerCitySub);
+        taxInvoiceForm.setSellerCity(this.sellerCity);
+        taxInvoiceForm.setSellerCountrySub(this.sellerCountrySub);
+        taxInvoiceForm.setSellerCountry(this.sellerCountry);
+        taxInvoiceForm.setSellerPostCode(this.sellerPostCode);
+
         assertEquals(this.id, taxInvoiceForm.getId());
         assertEquals(this.name, taxInvoiceForm.getName());
         assertEquals(this.typeCode, taxInvoiceForm.getTypeCode());
@@ -42,5 +77,23 @@ public class TaxInvoiceFormTest {
         assertEquals(this.globalId, taxInvoiceForm.getGlobalId());
         assertEquals(this.subject, taxInvoiceForm.getSubject());
         assertEquals(this.content, taxInvoiceForm.getContent());
+
+        assertEquals(this.sellerId, taxInvoiceForm.getSellerId());
+        assertEquals(this.sellerGlobalId, taxInvoiceForm.getSellerGlobalId());
+        assertEquals(this.sellerName, taxInvoiceForm.getSellerName());
+        assertEquals(this.sellerTaxRegId, taxInvoiceForm.getSellerTaxRegId());
+        assertEquals(this.sellerBranchId, taxInvoiceForm.getSellerBranchId());
+        assertEquals(this.sellerEmail, taxInvoiceForm.getSellerEmail());
+        assertEquals(this.sellerBuilding, taxInvoiceForm.getSellerBuilding());
+        assertEquals(this.sellerBuildingNum, taxInvoiceForm.getSellerBuildingNum());
+        assertEquals(this.sellerSoi, taxInvoiceForm.getSellerSoi());
+        assertEquals(this.sellerVillage, taxInvoiceForm.getSellerVillage());
+        assertEquals(this.sellerMoo, taxInvoiceForm.getSellerMoo());
+        assertEquals(this.sellerStreet, taxInvoiceForm.getSellerStreet());
+        assertEquals(this.sellerCitySub, taxInvoiceForm.getSellerCitySub());
+        assertEquals(this.sellerCity, taxInvoiceForm.getSellerCity());
+        assertEquals(this.sellerCountrySub, taxInvoiceForm.getSellerCountrySub());
+        assertEquals(this.sellerCountry, taxInvoiceForm.getSellerCountry());
+        assertEquals(this.sellerPostCode, taxInvoiceForm.getSellerPostCode());
     }
 }
