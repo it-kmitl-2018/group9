0ee14ad3bc0c81ccbe7b8669f2c9044c5a63815b
tiwipabmin <tiwipabmin@gmail.com>
2018-05-14 22:47:39 +0700

    Add function shouldGetCorrectXml in AddressXmlTest class

--- src/test/java/th/ac/kmitl/soa/group9/taxinvoice/models/xml/AddressXmlTest.java
+++ src/test/java/th/ac/kmitl/soa/group9/taxinvoice/models/xml/AddressXmlTest.java
@@ -1,6 +1,7 @@
 package th.ac.kmitl.soa.group9.taxinvoice.models.xml;
 
 import org.junit.Test;
+import th.ac.kmitl.soa.group9.taxinvoice.converters.XmlConverter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,10 +18,26 @@ public class AddressXmlTest {
     private String countryId = "TH";
     private String countrySubDivisionId = "10";
     private String buildingNumber = "799/89";
+    private AddressXml addressXml;
+
+    private String xml =
+            "<AddressXml>" +
+                    "<post_code>10330</post_code>" +
+                    "<building_name></building_name>" +
+                    "<line_3>จุฬาซอย 10</line_3>" +
+                    "<line_4></line_4>" +
+                    "<line_5></line_5>" +
+                    "<street_name>พระราม 6</street_name>" +
+                    "<city_name>1004</city_name>" +
+                    "<city_sub_division_name>100402</city_sub_division_name>" +
+                    "<country_id>TH</country_id>" +
+                    "<country_sub_division_id>10</country_sub_division_id>" +
+                    "<building_number>799/89</building_number>" +
+            "</AddressXml>";
 
     @Test
     public void shouldGetAllInfo() {
-        AddressXml addressXml = AddressXml.builder()
+        addressXml = AddressXml.builder()
                 .postCode(postCode)
                 .buildingName(buildingName)
                 .soi(soi)
@@ -45,5 +62,25 @@ public class AddressXmlTest {
         assertEquals(this.countryId, addressXml.countryId);
         assertEquals(this.countrySubDivisionId, addressXml.countrySubDivisionId);
         assertEquals(this.buildingNumber, addressXml.buildingNumber);
+    }
+
+    @Test
+    public void shouldGetCorrectXml() {
+        XmlConverter xmlConverter = new XmlConverter();
+        addressXml = AddressXml.builder()
+                .postCode(postCode)
+                .buildingName(buildingName)
+                .soi(soi)
+                .village(village)
+                .moo(moo)
+                .streetName(streetName)
+                .cityName(cityName)
+                .citySubDivisionName(citySubDivisionName)
+                .countryId(countryId)
+                .countrySubDivisionId(countrySubDivisionId)
+                .buildingNumber(buildingNumber)
+                .build();
+
+        assertEquals(xml, xmlConverter.toXml(addressXml));
     }
 }
