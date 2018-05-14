cebba48680f5a2e7d64b215e0fd9f3839c7af62c
iamplaster <iamaplaster@gmail.com>
2018-05-14 23:49:57 +0700

    Refactoring model with lombok

--- src/main/java/th/ac/kmitl/soa/group9/taxinvoice/models/interpreter/ProductCategory.java
+++ src/main/java/th/ac/kmitl/soa/group9/taxinvoice/models/interpreter/ProductCategory.java
@@ -1,34 +1,16 @@
 package th.ac.kmitl.soa.group9.taxinvoice.models.interpreter;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Builder
 public class ProductCategory {
 
     private String code;
     private String name;
-
-    public ProductCategory() {
-    }
-
-    public ProductCategory(String code, String name) {
-        this.code = code;
-        this.name = name;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
