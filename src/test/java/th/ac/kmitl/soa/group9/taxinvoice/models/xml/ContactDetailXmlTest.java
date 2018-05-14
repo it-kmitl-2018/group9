ace049f28cde6343d253141c8b836b33ee41843b
tiwipabmin <tiwipabmin@gmail.com>
2018-05-14 23:04:01 +0700

    Add function shouldGetCorrectXml in ContactDetailXmlTest

--- src/test/java/th/ac/kmitl/soa/group9/taxinvoice/models/xml/ContactDetailXmlTest.java
+++ src/test/java/th/ac/kmitl/soa/group9/taxinvoice/models/xml/ContactDetailXmlTest.java
@@ -1,6 +1,8 @@
 package th.ac.kmitl.soa.group9.taxinvoice.models.xml;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import th.ac.kmitl.soa.group9.taxinvoice.converters.XmlConverter;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,10 +12,18 @@ public class ContactDetailXmlTest {
     private String departmentName = "IT";
     private String email = "58070128@kmitl.ac.th";
     private String phoneNumber = "0812223333";
+    private ContactDetailXml contactDetailXml;
+    private String xml =
+            "<ContactDetailXml>" +
+                    "<person_name>วีราพัชร์ มีลาภ</person_name>" +
+                    "<department_name>IT</department_name>" +
+                    "<uri_id>58070128@kmitl.ac.th</uri_id>" +
+                    "<phone_number>0812223333</phone_number>" +
+            "</ContactDetailXml>";
 
     @Test
     public void shouldGetAllInfo() {
-        ContactDetailXml contactDetailXml = ContactDetailXml.builder()
+        contactDetailXml = ContactDetailXml.builder()
                 .personName(personName)
                 .departmentName(departmentName)
                 .email(email)
@@ -24,5 +34,18 @@ public class ContactDetailXmlTest {
         assertEquals(this.departmentName, contactDetailXml.departmentName);
         assertEquals(this.email, contactDetailXml.email);
         assertEquals(this.phoneNumber, contactDetailXml.phoneNumber);
+    }
+
+    @Test
+    public void shouldGetCorrectXml() {
+        XmlConverter xmlConverter = new XmlConverter();
+        contactDetailXml = ContactDetailXml.builder()
+                .personName(personName)
+                .departmentName(departmentName)
+                .email(email)
+                .phoneNumber(phoneNumber)
+                .build();
+
+        Assertions.assertEquals(xml, xmlConverter.toXml(contactDetailXml));
     }
 }
